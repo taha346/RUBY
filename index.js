d9922ce52721afa6911fcd1b377942acbc4aa5ec
taha346 <83064818+taha346@users.noreply.github.com>
2021-04-23 11:34:08 +0200

    intial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(process.env.TOKEN)
+client.commands = new Discord.Collection()
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member}`, new Discord.MessageEmbed()
+        .setDescription(`**${member}** a rejoint le serveur. Nous sommes désormais **${member.guild.memberCount}** ! 🎉`)
+        .setColor('#4887e6')
+        .setImage('https://media.giphy.com/media/13Uqp5IGFpmDle/giphy.gif'))
+        
+})
